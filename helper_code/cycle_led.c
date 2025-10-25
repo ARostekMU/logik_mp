d1f2ffddc14d833df0ade267cd30e06f20fd4b1a
Andrej Roštek <rostek@mail.muni.cz>
2025-10-25 17:02:18 +0200

    ADC cycling

--- helper_code/cycle_led.c
+++ helper_code/cycle_led.c
@@ -1,26 +1,37 @@
+#define F_CPU 16000000UL
 #include <util/delay.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include "light_ws2812.h"
+
+#define NUM_LEDS 4
 
 struct cRGB led[4];
 
-int main(void) {
-    DDRB |= (1 << DDB0)
+static inline void initADC5(void) {
+    ADMUX |= (1 << REFS0);                                              /* reference voltage on AVCC */
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);  /* ADC clock prescaler /128 */
+    ADCSRA |= (1 << ADEN);                                              /* enable ADC */
+}
 
-    led[1].r=80;led[2].g=80;led[3].b=80;    // Write red to array
+int main(void) {
+    DDRB |= (1 << DDB0);    // DI for LED strip
+
+    initADC5();
+
+    uint8_t ledValue;
+    uint16_t adcValue;
+
+    led[1].r=80;led[2].g=80;led[3].b=80;
 
     while(1) {
-        led[0].r=120;led[0].g=00;led[0].b=0;    // Write red to array
-        ws2812_setleds(led,1);
-        _delay_ms(500);                         // wait for 500ms.
+        ADCSRA |= (1 << ADSC);                     /* start ADC conversion */
+        loop_until_bit_is_clear(ADCSRA, ADSC);          /* wait until done */
+        adcValue = ADC;                                     /* read ADC in */
+        ledValue = (adcValue >> 2); // from 1024 to 256 range
 
-        led[0].r=0;led[0].g=120;led[0].b=0;			// green
-        ws2812_setleds(led,1);
-        _delay_ms(500);
+        led[0].r=ledValue;    // LED intensity red
+        ws2812_setleds(led, NUM_LEDS);
 
-        led[0].r=0;led[0].g=00;led[0].b=120;		// blue
-        ws2812_setleds(led,1);
-        _delay_ms(500);
+        _delay_ms(50);
     }
 }
