cc57d4ba58437b2432df3bc2a5ac6381feaf6e1b
Andrej Roštek <rostek@mail.muni.cz>
2025-10-25 16:14:04 +0200

    multiple leds

--- helper_code/cycle_led.c
+++ helper_code/cycle_led.c
@@ -3,22 +3,24 @@
 #include <avr/interrupt.h>
 #include "light_ws2812.h"
 
-struct cRGB led[1];
+struct cRGB led[4];
 
-int main(void)
-{
-  while(1)
-  {
-    led[0].r=255;led[0].g=00;led[0].b=0;    // Write red to array
-    ws2812_setleds(led,1);
-    _delay_ms(500);                         // wait for 500ms.
+int main(void) {
+    DDRB |= (1 << DDB0)
 
-    led[0].r=0;led[0].g=255;led[0].b=0;			// green
-    ws2812_setleds(led,1);
-    _delay_ms(500);
+    led[1].r=80;led[2].g=80;led[3].b=80;    // Write red to array
 
-    led[0].r=0;led[0].g=00;led[0].b=255;		// blue
-    ws2812_setleds(led,1);
-    _delay_ms(500);
-  }
+    while(1) {
+        led[0].r=120;led[0].g=00;led[0].b=0;    // Write red to array
+        ws2812_setleds(led,1);
+        _delay_ms(500);                         // wait for 500ms.
+
+        led[0].r=0;led[0].g=120;led[0].b=0;			// green
+        ws2812_setleds(led,1);
+        _delay_ms(500);
+
+        led[0].r=0;led[0].g=00;led[0].b=120;		// blue
+        ws2812_setleds(led,1);
+        _delay_ms(500);
+    }
 }
